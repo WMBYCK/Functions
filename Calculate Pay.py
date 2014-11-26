a579c05859b31bc0a272d1ef035ef485f6794260
44224 <44224@PC004145.coll.lr.local>
2014-11-26 15:17:27 +0000

    adsa

--- Calculate Pay.py
+++ Calculate Pay.py
@@ -1,3 +1,25 @@
+# calculate pay
+
+def calculate_pay(hour,rate,total):
+    
+    return hour,rate,total
+
+# get hours + rate of pay
+
+def get_hours_rate():
+    hours = int(input("Please enter the number of hours you work a week: "))
+    rate = int(input("Please enter the rate of pay you get per hour in pounds: £"))
+    return hours,rate
+
+# calculate total pay
+
+def calculate_total_pay(hours,rate):
+    if hours <= 40:
+        total = calculate_basic_pay(hours,rate)
+    else:
+        total = calculate_overtime_pay(hours,rate)
+    return total
+
 # calculate basic pay
 
 def calculate_basic_pay(hours,rate):
@@ -7,25 +29,11 @@ def calculate_basic_pay(hours,rate):
 # calculate overtime pay
 
 def calculate_overtime_pay(hours,rate):
-    total = ((hours - 40) * (rate * 1.5)) + (pay * 40)
+    total = ((hours - 40) * (rate * 1.5)) + (rate * 40)
     return total
 
-# calculate total pay
-def calculate_total_pay(hours,rate):
-    if hours <= 40:
-        total_pay = calculate_basic_pay(hours,rate)
-    else:
-        total_pay = calculate_overtime_pay(hours,rate)
-    return total_pay
+# display total pay
+def display_total_pay(total):
+    print(total)
 
 
-# main program
-
-total_pay = calculate_basic_pay(12,2)
-print(total_pay)
-
-total_pay = calculate_overtime_pay(41,1)
-print(total_pay)
-
-total_pay = calculate_total_pay(40,1)
-print(total_pay)
