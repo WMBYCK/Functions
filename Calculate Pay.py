f671039149d8b8ab23aa99ad3d872d796c6af6ae
44224 <44224@PC004145.coll.lr.local>
2014-12-01 09:55:14 +0000

    10/12

--- Calculate Pay.py
+++ Calculate Pay.py
@@ -1,17 +1,11 @@
-# calculate pay
-
-def calculate_pay(hour,rate,total):
-    
-    return hour,rate,total
-
-# get hours + rate of pay
+# Get Hours + Rate Of Pay
 
 def get_hours_rate():
     hours = int(input("Please enter the number of hours you work a week: "))
     rate = int(input("Please enter the rate of pay you get per hour in pounds: £"))
     return hours,rate
 
-# calculate total pay
+# Calculate Total Pay
 
 def calculate_total_pay(hours,rate):
     if hours <= 40:
@@ -20,20 +14,29 @@ def calculate_total_pay(hours,rate):
         total = calculate_overtime_pay(hours,rate)
     return total
 
-# calculate basic pay
+# Calculate Basic Pay
 
 def calculate_basic_pay(hours,rate):
     total = hours * rate
     return total
 
-# calculate overtime pay
+# Calculate Overtime Pay
 
 def calculate_overtime_pay(hours,rate):
     total = ((hours - 40) * (rate * 1.5)) + (rate * 40)
     return total
 
-# display total pay
+# Display Total Pay
 def display_total_pay(total):
-    print(total)
+    print("You make £{0}".format (total))
 
+# Calculate Pay
 
+def calculate_pay():
+    hours,rate = get_hours_rate()
+    total = calculate_total_pay(hours,rate)
+    display_total_pay(total)
+
+# Main Program
+
+calculate_pay()
