bb67966277f9771e557ef35a45098b55f487e078
44224 <44224@PC004145.coll.lr.local>
2014-12-03 15:19:05 +0000

    whjhgfdsfgk

---
+++ functions_improvement_ex_2.py
@@ -0,0 +1,12 @@
+
+# answer right
+
+def answer_right():
+    print('Well done, you got the correct answer!')
+    print()
+
+# answer wrong
+
+def answer_wrong(actualAnswer):
+    print('Sorry, you got the answer wrong. The correct answer is',Ans)
+    print()
